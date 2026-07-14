cee5e98ed2e24a78a98228b2c4d9ca8554ec6d82
Tarabass <Tarabass@users.noreply.github.com>
2026-07-14 10:47:20 +0200

    Add pages to test endpoints

---
+++ app/pages/contact.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const cookie = useCookie('myCookie')
+const { data, error } = await useFetch('/api/contactCall')
+</script>
+
+<template>
+  <div>
+    Data:{{ data }}<br />
+    Error:{{ error }}<br />
+    Cookie:{{ cookie }}<br />
+    <NuxtLink to="/">Home</NuxtLink> |
+    <NuxtLink to="/contact">Contact</NuxtLink>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const cookie = useCookie('myCookie')
+const { data, error, execute } = await useFetch('/api/indexCall', {
+  immediate: false,
+})
+</script>
+
+<template>
+  <div>
+    Data:{{ data }}<br />
+    Error:{{ error }}<br />
+    Cookie:{{ cookie }}<br />
+    <button @click="execute()">Fetch Data</button><br />
+    <NuxtLink to="/">Home</NuxtLink> |
+    <NuxtLink to="/contact">Contact</NuxtLink>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
